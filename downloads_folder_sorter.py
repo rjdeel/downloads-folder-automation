b1e33dbd310a7a0d2b120943b5df2325d4db3633
rjdeel <contact@rdeel.tech>
2023-09-12 11:06:21 -0500

    formatting and personalization modifications

--- downloads_folder_sorter.py
+++ downloads_folder_sorter.py
@@ -38,23 +38,23 @@ def sort_folder(folder_path):
     folder_path : Path
         the path to the folder to be organized
     """
-    with open('config.json', encoding='utf-8') as f:
+    with open("config.json", encoding="utf-8") as f:
         categories = json.load(f)
 
     extensions_map = {}
     for category in categories:
-        folder_name = category['name']
-        for extension in category['extensions']:
+        folder_name = category["name"]
+        for extension in category["extensions"]:
             extensions_map[extension] = folder_name
 
     for file in folder_path.iterdir():
-        if file.is_file() and not file.name.startswith('.'):
-            destination = extensions_map.get(file.suffix, 'Other')
+        if file.is_file() and not file.name.startswith("."):
+            destination = extensions_map.get(file.suffix, "Other")
             move_file(file, file.parent.joinpath(destination))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     home_directory = str(Path.home())
-    downloads_path = Path(f'{home_directory}/Downloads')
+    downloads_path = Path(f"{home_directory}/Downloads")
 
     sort_folder(downloads_path)
